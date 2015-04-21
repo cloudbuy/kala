4fc094c934795279939211a93a63ad2a8f54ef49
Paul J. Etherton <paul@pjetherton.co.uk>
2015-04-21 17:40:07 +0100

    Added a simple setup.py file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='kala',
+    version='0.1dev',
+    packages=[''],
+    url='https://github.com/cloudbuy/kala',
+    license='MIT',
+    author='Paul Etherton',
+    author_email='paul@pjetherton.co.uk',
+    description='Simple read-only REST API for mongoDB',
+    requires=[
+        'bottle',
+        'bottle_mongodb',
+        'pymongo'
+    ]
+)
