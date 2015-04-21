aa2490f7a37a1859f65a626feace0728c3d52f56
Paul J. Etherton <paul@pjetherton.co.uk>
2015-04-21 15:40:00 +0100

    First attempt.
    Currently untested.

---
+++ kala.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import json
+
+import bottle
+from bottle_mongo import MongoPlugin
+
+
+app = bottle.Bottle()
+
+app.config.load_config('settings.ini')
+
+bottle.install(MongoPlugin(
+    uri=app.config('mongodb.uri'),
+    db=app.config('mongodb.db'),
+    json_mongo=True))
+
+
+def _get_json(name):
+    result = bottle.request.get(name)
+    return json.loads(result) if result else None
+
+
+@app.route('/<collection>')
+def get(mongodb, collection):
+    filter_ = _get_json('filter')
+    projection = _get_json('projection')
+    skip = int(bottle.request.get('skip', '0'))
+    limit = int(bottle.request.get('limit', '100'))
+    sort = _get_json('sort')
+
+    cursor = mongodb[collection].find(
+        filter=filter_, projection=projection, skip=skip, limit=limit,
+        sort=sort
+    )
+
+    return {'results': [document for document in cursor]}
+
+
+if __name__ == '__main__':
+    app.run()
