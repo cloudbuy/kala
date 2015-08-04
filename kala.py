c056adfe89812c5e2a186696c3bbe71d667b1382
derek.cheng <derek.cheng@cloudbuy.com>
2015-08-04 15:12:03 +0100

    _filter_read

--- kala.py
+++ kala.py
@@ -25,22 +25,45 @@ def _get_json(name):
     return json.loads(result) if result else None
 
 
-def _filter_write(mongodb, dictionary, filter_json):
-    if filter_json is None:
+def _filter_write(mongodb, dictionary):
+    if app.config['filter.json'] is None:
         return dictionary
     staging = app.config.get('filter.staging', 'staging')
     object_id = mongodb[staging].insert(dictionary)
-    cursor = mongodb[staging].find(filter=filter_json)
+    cursor = mongodb[staging].find(filter=app.config['filter.json'])
+    # Delete from staging collection after cursor becomes a list otherwise, cursor will produce an empty list.
     documents = [document for document in cursor]
     mongodb[staging].remove({"_id":object_id}, "true")
     return documents
 
 
-def _filter_read(dictionary, whitelist):
+def _filter_read(var):
+    whitelist = app.config['filter.read']
     if whitelist is None:
-        return dictionary
+        return var
+    if isinstance(var, dict):
+        for key in list(var.keys()):
+            if key.startswith('$'):
+                _filter_read(var[key])
+            elif key not in whitelist:
+                del var[key]
+    elif isinstance(var, list):
+        var[:] = [item for item in var if _filter_read(item)]
+    elif isinstance(var, tuple):
+        return var[0] in whitelist
+    elif var is None:
+        var = dict((key,'1') for key in whitelist)
+    else:
+        return
+    return var
 
-    return dictionary
+
+@app.route('/aggregate/<collection>')
+def get_aggregate(mongodb, collection):
+    pipeline = _get_json('pipeline')
+    pipeline = _filter_read(pipeline) if pipeline else pipeline
+    cursor = mongodb[collection].aggregate(pipeline=pipeline)
+    return {'results': [document for document in cursor]}
 
 
 @app.route('/<collection>')
@@ -51,17 +74,20 @@ def get(mongodb, collection):
     limit = int(bottle.request.query.get('limit', 100))
     sort = _get_json('sort')
 
-    # We use a whitelist read setting to filter what is allowed to be read from the collection.
-    # If the whitelist read setting is empty or non existent, then nothing is filtered.
-    if 'whitelist.read' in app.config:
-        filter_ = _filter_read(filter_)
-        projection = _filter_read(projection)
-        sort = _filter_read(sort)
-
     # Turns a list of lists to a list of tuples.
     # This is necessary because JSON has no concept of "tuple" but pymongo
     # takes a list of tuples for the sort order.
     sort = [tuple(field) for field in sort] if sort else None
+
+    # We use a whitelist read setting to filter what is allowed to be read from the collection.
+    # If the whitelist read setting is empty or non existent, then nothing is filtered.
+    if 'filter.read' in app.config:
+        filter_ = _filter_read(filter_) if filter_ else filter_
+        # Filter must be applied to projection, this is to prevent unrestricted reads.
+        # If it is empty, we fill it with only whitelisted values.
+        # Else we remove values which are not whitelisted.
+        projection = _filter_read(projection)
+        sort = _filter_read(sort) if sort else sort
 
     cursor = mongodb[collection].find(
         filter=filter_, projection=projection, skip=skip, limit=limit,
@@ -83,7 +109,7 @@ def post(mongodb, collection):
     # If no filter JSON document is defined in the configuration setting, then write access is disabled.
     if 'filter.json' in app.config:
         json_ = bottle.request.json
-        if _filter_write(mongodb, json_, app.config['filter.json']):
+        if _filter_write(mongodb, json_):
             mongodb[collection].insert(json_)
 
 
@@ -94,9 +120,9 @@ def main():
 if 'filter.json' in app.config:
     with open(app.config['filter.json']) as data_file:
         app.config['filter.json'] = json.load(data_file)
-if 'whitelist.read' in app.config:
-    app.config['whitelist.read'] = app.config['whitelist.read'].split(',') \
-        if app.config['whitelist.read'] else None
+if 'filter.read' in app.config:
+    app.config['filter.read'] = app.config['filter.read'].split(',') \
+        if app.config['filter.read'] else None
 
 if __name__ == '__main__':
     main()
