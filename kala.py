113f4303a5863805ec70d5aa38c31bd57e784a37
Charlotte Seabrook <charlotte.seabrook@cloudbuy.com>
2016-03-14 16:18:03 +0000

    Added the ability to request a count of documents matching the current filter.

--- kala.py
+++ kala.py
@@ -73,6 +73,9 @@ def get(mongodb, collection):
     if distinct:
         return {'values': cursor.distinct(distinct)}
 
+    if bottle.request.query.get('count'):
+        return {'count': cursor.count()}
+
     return {'results': [document for document in cursor]}
 
 
